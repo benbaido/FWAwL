9761e63204a85d9efaa9fd45ef579ccccde521d5
bene <mor3thanb3for3@gmail.com>
2015-07-23 03:18:03 -0700

    fixed nav collapse...for real this time.

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,18 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $navbar = $('#inside_nav');
-    
-    
-    $(window).resize(function(){
-        if(window.innerWidth < 768) {
-            if(!$navbar.hasClass('collapse')) {
+
+    if ((window.innerWidth < 768) && (!$navbar.hasClass('collapse'))) {
+        $navbar.addClass('collapse');
+    } else if ((window.innerWidth >= 768) && ($navbar.hasClass('collapse'))) {
+        $navbar.removeClass('collapse');
+    }
+
+    $(window).resize(function () {
+        if (window.innerWidth < 768) {
+            if (!$navbar.hasClass('collapse')) {
                 $navbar.addClass('collapse');
             }
-        }
-        else if(window.innerWidth >= 768) {
-            if($navbar.hasClass('collapse')) {
+        } else if (window.innerWidth >= 768) {
+            if ($navbar.hasClass('collapse')) {
                 $navbar.removeClass('collapse');
             }
         }
     });
-     
+
 });
